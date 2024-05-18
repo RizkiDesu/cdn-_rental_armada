7c403bfad5d9cd25f6a056f5c20dfce86f928e3b
RIZKI DESU <s4y4pu1an6@gmail.com>
2024-05-18 11:51:18 +0700

    Menu [rizki]

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "CDN Rental Armada",
+    'name': "cdn_rental_armada",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -10,14 +10,14 @@
         Long description of module's purpose
     """,
 
-    'author': "Rental Armada",
-    'website': "https://www.cendana2000.com",
+    'author': "My Company",
+    'website': "https://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '16.0.1',
+    'version': '0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
@@ -25,6 +25,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
+        'views/menu_view.xml',
         'views/views.xml',
         'views/templates.xml',
     ],
