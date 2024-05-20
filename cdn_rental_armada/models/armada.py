d40035bb706efacdde8d3d90fa25a2c23a14da9b
evosasori <triadisputra123@gmail.com>
2024-05-20 10:00:46 +0700

    menambahkan ribbon dan sql constraint[Triadi]

--- cdn_rental_armada/models/armada.py
+++ cdn_rental_armada/models/armada.py
@@ -3,17 +3,21 @@ from odoo import _, api, fields, models
 
 #adi
 class CdnArmada(models.Model):
-    _name = 'cdn.armada'
-    _description = 'Cdn Armada'
+    _name            = 'cdn.armada'
+    _description     = 'Armada'
+    _sql_constraints = [
+        ('unique_no_plat', 'Unique(no_plat)','Nomor polisi tidak boleh sama!'),
+        ('unique_no_mesin', 'Unique(no_mesin)','Nomor mesin tidak boleh sama!'),
+    ]
 
-    merek_id = fields.Many2one(comodel_name='cdn.merek', string='merek kendaraan',required=True)
+    merek_id        = fields.Many2one(comodel_name='cdn.merek', string='merek kendaraan',required=True)
     jenis_kendaraan = fields.Many2one(comodel_name='cdn.jenis.kendaraan', string='Jenis kendaraan',required=True,domain="[('merek_id', '=', merek_id)]")
-    jumlah_kursi = fields.Integer(string='Jumlah Kursi', required=True)
-    jenis_armada = fields.Selection(string='Jenis Armada', selection=[('bis', 'Bis Pariwisata'), ('travel', 'Travel'),('mobil', 'Mobil')], required=True)    
+    jumlah_kursi    = fields.Integer(string='Jumlah Kursi', required=True)
+    jenis_armada    = fields.Selection(string='Jenis Armada', selection=[('bis', 'Bis Pariwisata'), ('travel', 'Travel'),('mobil', 'Mobil')], required=True)    
     tahun_pembuatan = fields.Date(string='Tahun Pembuatan', required=True)
-    no_plat = fields.Char(string='Plat Nomor', required=True)
-    no_mesin = fields.Char(string='No Rangka & No Mesin',required=True)
-    kondisi = fields.Boolean(string='Kondisi Armada', default="True")
+    no_plat         = fields.Char(string='Plat Nomor', required=True)
+    no_mesin        = fields.Char(string='No Rangka & No Mesin',required=True)
+    kondisi         = fields.Boolean(string='Kondisi Armada', default="True")
     
     def name_get(self):
         return [(record.id, "[ %s ] %s %s" % (record.jenis_armada, record.merek_id.name, record.jenis_kendaraan.name)) for record in self]
@@ -22,8 +26,8 @@ class CdnArmada(models.Model):
     def create(self, vals):
         if 'jenis_kendaraan' in vals and isinstance(vals['jenis_kendaraan'], str):
             jenis_kendaraan_name = vals['jenis_kendaraan']
-            merek_id = vals.get('merek_id')
-            jenis_kendaraan = self.env['cdn.jenis.kendaraan'].create({
+            merek_id             = vals.get('merek_id')
+            jenis_kendaraan      = self.env['cdn.jenis.kendaraan'].create({
                 'name': jenis_kendaraan_name,
                 'merek_id': merek_id,
             })
