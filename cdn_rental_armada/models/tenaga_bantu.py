20bdad379763fa37abc9976b81a803b88508efaf
Rizki Desu <s4y4pu1an6@gmail.com>
2024-05-21 13:05:11 +0700

    update supir[rizki]

--- cdn_rental_armada/models/tenaga_bantu.py
+++ cdn_rental_armada/models/tenaga_bantu.py
@@ -11,17 +11,17 @@ class TenagaKerja(models.Model):
     umur          = fields.Integer(string='Umur', required=True)
     is_menikah    = fields.Boolean(string='Menikah', default=False, required=True)
     status        = fields.Selection(string='Status', selection=[('aktif', 'Aktif'), ('nonaktif', 'Nonaktif'),], default="aktif", required=True)
-    sim_line      = fields.One2many(comodel_name='cdn.sim', inverse_name='sim_id', string='Izin Mengemudi')
+    # sim_line      = fields.One2many(comodel_name='cdn.sim', inverse_name='sim_id', string='Izin Mengemudi')
 
 
-class CdnSim(models.Model):
-    _name         = 'cdn.sim'
-    _description  = 'cdn.sim'
+# class CdnSim(models.Model):
+#     _name         = 'cdn.sim'
+#     _description  = 'cdn.sim'
 
-    sim_id      = fields.Many2one(comodel_name='res.partner', string='Partner')
-    name        = fields.Char(string='Nama', required=True)
-    no_sim      = fields.Char(string='No Sim', required=True)
-    jenis_sim   = fields.Selection(string='Jenis Sim', selection=[('a', 'Sim A'), ('b', 'Sim B')], required=True) 
-    foto_sim    = fields.Image('Foto Sim', max_width=60, required=True)
+#     sim_id      = fields.Many2one(comodel_name='res.partner', string='Partner')
+#     name        = fields.Char(string='Nama', required=True)
+#     no_sim      = fields.Char(string='No Sim', required=True)
+#     jenis_sim   = fields.Selection(string='Jenis Sim', selection=[('a', 'Sim A'), ('b', 'Sim B')], required=True) 
+#     foto_sim    = fields.Image('Foto Sim', max_width=60, required=True)
 
 
