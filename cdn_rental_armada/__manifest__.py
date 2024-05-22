2dab9fa45a78e4f9d05cdb1d99fbc450f400e8d9
Rizki Desu <s4y4pu1an6@gmail.com>
2024-05-22 11:36:39 +0700

    merapikan manifesh.py [rizki]

--- cdn_rental_armada/__manifest__.py
+++ cdn_rental_armada/__manifest__.py
@@ -1,28 +1,18 @@
-# -*- coding: utf-8 -*-
+
 {
-    'name': "Rental Armada",
+    'name'          : 'cdn_rental_armada',
+    'version'       : '1.0.0',
+    'summary'       : """odoo 16""",
+    'description'   : """Membuat module odoo16 Rental Armada""",
+    'category'      : 'rental_armada',
+    'author'        : 'Armada Team',
+    'maintainer'    : 'armada_team',
+    'website'       : 'https://www.website.com',
+    'license'       : 'AGPL-3',
+    'aplication'    : True,
 
-    'summary': """
-        Modul ini digunakan untuk mengelola data armada rental kendaraan 
-        """,
-
-    'description': """
-        Aplikasi ini digunakan untuk mengelola data armada rental kendaraan dalam tugas cdn training
-    """,
-
-    'author': "CDN ARMADA TEAM",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'rental armada',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base', 'product', 'account'],
-
-    # always loaded
+    'depends'       : ['base', 'product', 'account'],
+    
     'data': [
         'security/group.xml',
         'security/ir.model.access.csv',
@@ -45,9 +35,6 @@
         'reports/bus_tersedia.xml',
         'views/pelanggan.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
-    'application': True,
+    
+
 }
