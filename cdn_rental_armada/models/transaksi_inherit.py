f7bd1d6423645a515c26dc44e2565ac05ce9ddd2
rayan0087 <rayansarijatmiko@gmail.com>
2024-05-24 10:17:29 +0700

    tambah onchange pada transaksi inherit [ian]

--- cdn_rental_armada/models/transaksi_inherit.py
+++ cdn_rental_armada/models/transaksi_inherit.py
@@ -6,9 +6,15 @@ class CdnSaleOrder(models.Model):
 
    jenis_armada   = fields.Selection(string='Jenis Armada', selection=[('bis', 'Bis Pariwisata'), ('travel', 'Travel'),('mobil', 'Mobil')], required=True)    
    
-   
+   @api.onchange('jenis_armada')
+   def _onchange_jenis_armada(self):
+      if self.jenis_armada:
+         self.order_line = [(5, 0, 0)]
+         
 class CdnSaleOrderLines(models.Model):
    _inherit = 'sale.order.line'
    
    supir          = fields.Many2one(comodel_name='cdn.supir', string='Supir')
    tenaga_bantu   = fields.Many2one(comodel_name='cdn.tenaga.bantu', string='Tenaga Bantu')
+
+
