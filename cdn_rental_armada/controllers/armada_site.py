becc66d74d1d971cb8604e8de893b69d3cba7df7
RIZKI DESU <s4y4pu1an6@gmail.com>
2024-06-08 12:33:09 +0700

    syncron [rizki]

--- cdn_rental_armada/controllers/armada_site.py
+++ cdn_rental_armada/controllers/armada_site.py
@@ -7,10 +7,17 @@ class ArmadaSite(http.Controller):
     def index(self, **kw):
         armadas = request.env['cdn.armada'].sudo().search([('priority', '=', 1)])
         return request.render('cdn_rental_armada.armada_list', {'armadas': armadas})
+        
+    @http.route('/booking' , auth='public', website=True)
+    def Produk(self, **kw):
+        # products = request.env['cdn.produk.armada'].sudo().search([('priority', '=', '1')])
+        # renderweb = request.render('cdn_rental_armada.product_booking', {'products': products})
+        # return renderweb
+        return "produk booking"
     
     @http.route('/' , auth='public', website=True)
     def home(self, **kw):
-        products = request.env['cdn.produk.armada'].sudo().search([])
+        products = request.env['cdn.produk.armada'].sudo().search([('priority', '=', '1')])
         renderweb = request.render('cdn_rental_armada.product_list', {'products': products})
         return renderweb
     
