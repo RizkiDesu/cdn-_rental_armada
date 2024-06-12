310dbe1490cc4e1dd6b4b1808c16ee2049b9604d
Rizki Putra Utama Endriansyah <61736957+RizkiDesu@users.noreply.github.com>
2024-06-13 02:47:30 +0700

    odoo website home page [rizki]

--- cdn_rental_armada/models/pengaturan.py
+++ cdn_rental_armada/models/pengaturan.py
@@ -2,6 +2,7 @@ from odoo import _, api, fields, models
 
 
 # CREATED BY TRIADI
+# UPDATE BY RIZKI
 # ------------------------------- PENGATURAN --------------------------------
 class ResConfigSetting(models.TransientModel):
     _inherit        = 'res.config.settings'
@@ -12,6 +13,9 @@ class ResConfigSetting(models.TransientModel):
     # ------------------------------- PENGATURAN WEBSITE HOME -----------------------------------
     slogan          = fields.Char(string='Slogan', config_parameter="cdn_rental_armada.slogan", default="Pilihan Tepat untuk Setiap Perjalanan.")
     deskripsi       = fields.Char(string='Deskripsi', config_parameter="cdn_rental_armada.deskripsi", default="deskripsi persewaan")
+    deskripsi_id  = fields.Many2one(comodel_name='cdn.deskripsi', string='deskripsi layanan', config_parameter="cdn_rental_armada.deskripsi_id")
+    
+    
     headline        = fields.Char(string='Headline', config_parameter="cdn_rental_armada.headline", default="50%")
     deskripsi_event = fields.Char(string='Deskripsi Event', config_parameter="cdn_rental_armada.event", default="event persewaan")
     tanggal_event   = fields.Datetime(string='Tanggal Event', config_parameter="cdn_rental_armada.tanggal_event")
@@ -20,13 +24,24 @@ class ResConfigSetting(models.TransientModel):
     url_api         = fields.Char(string='URL API', config_parameter="cdn_rental_armada.url_api" , default="http://localhost:8069/virtual_account/create")
 
 
+
+
+class CdnDeskripsi(models.Model):
+    _name = 'cdn.deskripsi'
+    _description = 'Cdn Deskripsi'
+
+    name = fields.Char(string='Nama')
+    # slogan = fields.Char(string='Slogan')
+    deskripsi = fields.Text(string='Deskripsi')
+
 class CdnYourService(models.Model):
     _name = 'cdn.your.service'
     _description = 'Your Service'
     
     name = fields.Char(string='Nama Layanan')
+    long_deskripsi = fields.Text(string='Deskripsi Layanan')
     
-    
+
 
     
     
